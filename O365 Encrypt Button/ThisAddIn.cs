944b45537f6e166addc9dd12c51f6c8388bad263
Michael Nieto <michaelnietotx@gmail.com>
2017-11-25 00:28:24 -0600

    Initial commit

---
+++ O365 Encrypt Button/ThisAddIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Interop.Outlook;
+
+
+namespace O365_Encrypt_Button
+{
+    public partial class ThisAddIn
+    {
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon1();
+        }
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            CreateRibbonExtensibilityObject();
+
+        }
+       
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see http://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+
+ 
+
+
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+       
+    }
+}
